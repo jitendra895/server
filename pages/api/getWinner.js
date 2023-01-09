4589e187b08b80844baf16a5c513732310371671
jitendrasen_09 <jitendersen8959@gmail.com>
2023-01-09 16:02:51 +0530

    h

--- pages/api/getWinner.js
+++ pages/api/getWinner.js
@@ -1,7 +1,27 @@
 import Winners from "../../models/winnerResult"
 import connectDb from "../../MongodbConn/mongodb";
+import Cors from 'cors'
 
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+function runMiddleware(
+  req,
+  res,
+  fn
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 const handler = async (req, res) => {
+  await runMiddleware(req, res, cors)
   let winner = await Winners.find();
   res.status(200).json({ winner });
 };
